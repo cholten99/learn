e35c8790df1821bc01d60043caeb12e121601819
David Durant <dave@bowsy.co.uk>
2017-04-01 01:38:53 +0100

    Got it working

--- python/json_procesing/json-processing.py
+++ python/json_procesing/json-processing.py
@@ -3,23 +3,23 @@ import json
 from pprint import pprint
 
 # Create file handle called nads_fh
-nads_fh = open("route.json","r")
+route_fh = open("route.json","r")
 
 # Create document object model called nads_dom by reading in data from file via file handle
-nads_dom = json.loads(nads_fh.read())
+route_dom = json.loads(route_fh.read())
+
+# Let's start by printing out what we're doing
+print "Bus routes"
 
 # Loop through the array that is the value-side of the top level name "bus_routes"
-# Each loop through puts a set into i in the form of a dict
-for i in nads_dom["bus_routes"]:
-  keys = i.keys()
-  for key in keys:
-    print key
-    
-    
-"""
-    for j in nads_dom[i]:
-        print " " + j + " : "# + nads_dom[i][j]
-        if j == "stop_name":
-            for k in nads_dom[i][j]:
-                 print "  " + k
-"""
+# Each loop through puts a set into top_level_sets in the form of a dict
+for top_level_set in route_dom["bus_routes"]:
+  print " Route name : " + top_level_set["route_name"]
+  print "  Route start time : " + top_level_set["route_start_time"]
+  print "  Route end time : " + top_level_set["route_end_time"]
+  print "  Stop names :"
+  # Loop through the stop names array
+  for stop in top_level_set["stop_names"]:
+    print "   Name : " + stop
+
+# THE NEXT CHALLANGE WOULD BE TO CREATE AN INSTANCE OF A ROUTE CLASS FOR EACH TOP LEVEL SET IN THE JSON FILE!
